9b421b1d11b2c96b3178fb44fd682301bf05bb8e
alex misyuk <glassfox@gmail.com>
2026-01-11 14:38:57 +0200

    init createAccount

--- opinion-account-common/src/main/java/com/inqwise/opinion/account/CreateRequest.java
+++ opinion-account-common/src/main/java/com/inqwise/opinion/account/CreateRequest.java
@@ -18,5 +18,45 @@ public class CreateRequest {
 
 		return json;
 	}
+
+	public String getClientIp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer getCountryId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer getSourceId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer getProductId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer getServicePackageId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Long getUserId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Long getBackofficeUserId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
